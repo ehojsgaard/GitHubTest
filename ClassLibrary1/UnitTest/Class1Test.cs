f8c40bdfe9d251fa13884305bae2c0b7969634fc
Espen Højsgaard <espen@hojsgaard.dk>
2013-02-03 20:24:19 +0100

    A test solution

---
+++ ClassLibrary1/UnitTest/Class1Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace ClassLibrary1.UnitTest
+{
+  [TestFixture]
+  [Category("UnitTest")]
+  class Class1Test
+  {
+    [Test]
+    public void FailingTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
